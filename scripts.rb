8fcd0977c1fb79ede1e8438222bc04dde71ee68c
Heark <hearkofsorcerey@gmail.com>
2014-11-20 22:28:54 -0500

    Update scripts.rb

--- scripts.rb
+++ scripts.rb
@@ -14,22 +14,25 @@ class Player
 end
 
 class Enemy
-  attr_accessor :name; :hp; :attack; :defense; :weapon;
-  def initialize(name, hp, attack, defense, weapon)
+  attr_accessor :name; :hp; :attack; :defense; :weapon; :golddrop;
+  def initialize(name, hp, attack, defense, weapon, golddrop)
     @name = name
     @hp = hp
     @attack = attack
     @defense = defense
     @weapon = weapon
+    @golddrop = golddrop
   end
 end
 # = Skills
 
 # Players & Enemies
 User = Player.new(100, 25, 15, nil)
-Slime = Enemy.new("Slime", 20, 5, 5, nil)
-Bat = Enemy.new("Bat", 40, 10, 10, nil)
-#Defining Items & weapons
+Slime = Enemy.new("Slime", 20, 5, 5, nil, 10)
+Bat = Enemy.new("Bat", 40, 10, 10, nil, 25)
+#Defining Items & weapons & inventory
+pouch = 0 # where gold is stored
+inventory = [];
 class Weapon
   attr_accessor :name; :damage;
   def initialize(name, damage)
